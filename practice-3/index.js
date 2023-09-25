d4307ef12708144bb98e1ff784ef1c9de6b72b9f
Maria-Ba-ra-no-va <129512407+Maria-Ba-ra-no-va@users.noreply.github.com>
2023-09-25 23:37:27 +0300

    add new files

--- practice-3/index.js
+++ practice-3/index.js
@@ -1,7 +1,7 @@
 (async function () {
 
-    const $container = document.getElementById('container'),
-    
+  const $container = document.getElementById('container'),
+
     $search = document.createElement('input'),
 
     $table = document.createElement('table'),
@@ -13,114 +13,120 @@
     $thHeadBody = document.createElement('th'),
     $tbody = document.createElement('tbody');
 
-    $thHeadId.textContent = 'id';
-    $thHeadUserId.textContent = 'userId';
-    $thHeadTitle.textContent = 'title';
-    $thHeadBody.textContent = 'body';
-    $search.setAttribute('placeholder', 'Введите запрос');
+  $thHeadId.textContent = 'id';
+  $thHeadUserId.textContent = 'userId';
+  $thHeadTitle.textContent = 'title';
+  $thHeadBody.textContent = 'body';
+  $search.setAttribute('placeholder', 'Введите запрос');
 
-    $container.append($search);
-    $container.append($table);
-    $table.append($thead);
-    $thead.append($trHead);
-    $trHead.append($thHeadId);
-    $trHead.append($thHeadUserId);
-    $trHead.append($thHeadTitle);
-    $trHead.append($thHeadBody);
-    $table.append($tbody);
+  $container.append($search);
+  $container.append($table);
+  $table.append($thead);
+  $thead.append($trHead);
+  $trHead.append($thHeadId);
+  $trHead.append($thHeadUserId);
+  $trHead.append($thHeadTitle);
+  $trHead.append($thHeadBody);
+  $table.append($tbody);
 
-    let dataList = [];
+  let dataList = [];
 
-    async function loadData() {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-        if (response.ok) {
-            let data = await response.json();
-            return data;
-        } else {
-            alert("Ошибка HTTP: " + response.status);
-        };
-    }
+  async function loadData() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    if (response.ok) {
+      let data = await response.json();
+      return data;
+    } else {
+      alert("Ошибка HTTP: " + response.status);
+    };
+  }
 
-    let sortColumn = '',
+  let sortColumn = '',
     sortDir = true;
 
-    async function renderDataTable(arrData) {
-        $tbody.innerHTML = '';
+  async function renderDataTable(arrData) {
+    $tbody.innerHTML = '';
 
-        arrData.sort(function (a, b) {
-            let sort = a[sortColumn] < b[sortColumn]
-            if (sortDir === false) sort = a[sortColumn] > b[sortColumn]
-            return sort ? -1 : 1
-          })
+    arrData.sort(function (a, b) {
+      let sort = a[sortColumn] < b[sortColumn]
+      if (sortDir === false) sort = a[sortColumn] > b[sortColumn]
+      return sort ? -1 : 1
+    })
 
-        //cоздаем строку 
-        for (let user of arrData) {
+    //cоздаем строку 
+    for (let user of arrData) {
 
-            const $line = document.createElement('tr'),
-                $IdTd = document.createElement('td'),
-                $UserIdTd = document.createElement('td'),
-                $TitleTd = document.createElement('td'),
-                $BodyTd = document.createElement('td');
+      const $line = document.createElement('tr'),
+        $IdTd = document.createElement('td'),
+        $UserIdTd = document.createElement('td'),
+        $TitleTd = document.createElement('td'),
+        $BodyTd = document.createElement('td');
 
-            $IdTd.classList.add('td-id');
-            $UserIdTd.classList.add('td-id');
+      $IdTd.classList.add('td-id');
+      $UserIdTd.classList.add('td-id');
 
-            $IdTd.textContent = user.id;
-            $UserIdTd.textContent = user.userId;
-            $TitleTd.textContent = user.title;
-            $BodyTd.textContent = user.body;
+      $IdTd.textContent = user.id;
+      $UserIdTd.textContent = user.userId;
+      $TitleTd.textContent = user.title;
+      $BodyTd.textContent = user.body;
 
-            $tbody.append($line);
-            $line.append($IdTd);
-            $line.append($UserIdTd);
-            $line.append($TitleTd);
-            $line.append($BodyTd);
-        }
+      $tbody.append($line);
+      $line.append($IdTd);
+      $line.append($UserIdTd);
+      $line.append($TitleTd);
+      $line.append($BodyTd);
     }
+  }
 
-    dataList = await loadData();
-    renderDataTable(dataList);
+  dataList = await loadData();
+  renderDataTable(dataList);
+  let dataListFilter = [];
 
-//сортировка
-    $thHeadId.addEventListener('click', function (event) {
-        event.preventDefault();
-        sortColumn = 'id';
-        renderDataTable(dataList);
-        sortDir = !sortDir;
-      })
+  //сортировка
 
-      $thHeadUserId.addEventListener('click', function (event) {
-        event.preventDefault();
-        sortColumn = 'userId';
-        renderDataTable(dataList);
-        sortDir = !sortDir;
-      })
+  function renderSort() {
+    if (dataListFilter.length == 0) {
+      dataListFilter = [...dataList];
+    }
+    renderDataTable(dataListFilter);
+    sortDir = !sortDir;
+  }
 
-      $thHeadTitle.addEventListener('click', function (event) {
-        event.preventDefault();
-        sortColumn = 'title';
-        renderDataTable(dataList);
-        sortDir = !sortDir;
-      })
+  $thHeadId.addEventListener('click', function (event) {
+    event.preventDefault();
+    sortColumn = 'id';
+    renderSort();
+  })
 
-      $thHeadBody.addEventListener('click', function (event) {
-        event.preventDefault();
-        sortColumn = 'body';
-        renderDataTable(dataList);
-        sortDir = !sortDir;
-      })
+  $thHeadUserId.addEventListener('click', function (event) {
+    event.preventDefault();
+    sortColumn = 'userId';
+    renderSort();
+  })
 
-      //фильтрация
-      $search.addEventListener('input', function () {
-        let dataListNew=[];
-        if ($search.value.length>2){
-    for(let i = 0; i < dataList.length; i++) {
-        if(dataList[i].id.toString().includes($search.value)||dataList[i].userId.toString().includes($search.value)||dataList[i].title.toString().includes($search.value)||dataList[i].body.toString().includes($search.value)) {
-          dataListNew.push(dataList[i]);
+  $thHeadTitle.addEventListener('click', function (event) {
+    event.preventDefault();
+    sortColumn = 'title';
+    renderSort();
+  })
+
+  $thHeadBody.addEventListener('click', function (event) {
+    event.preventDefault();
+    sortColumn = 'body';
+    renderSort();
+  })
+
+  //фильтрация
+  $search.addEventListener('input', function () {
+    dataListFilter = [];
+    if ($search.value.length > 2) {
+      for (let i = 0; i < dataList.length; i++) {
+        if (dataList[i].id.toString().includes($search.value) || dataList[i].userId.toString().includes($search.value) || dataList[i].title.toString().includes($search.value) || dataList[i].body.toString().includes($search.value)) {
+          dataListFilter.push(dataList[i]);
         }
       }
-    renderDataTable(dataListNew);
-        }   
-      })
+      renderDataTable(dataListFilter);
+    }
+  })
 
 })();
